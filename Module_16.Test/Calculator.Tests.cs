2d672db4be440b0160011b70dad401ce6d21ba34
HoStWgSp <hostwg@vk.com>
2024-06-21 19:16:18 +1100

    16.6.1. Практика (HW-03)

---
+++ Module_16.Test/Calculator.Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+
+namespace Module_16.Test
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Additional(50, 10);
+            ClassicAssert.AreEqual(60, result);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Miltiplication(3, 2);
+            ClassicAssert.AreEqual(6, result);
+        }
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Subtraction(3, 2);
+            ClassicAssert.AreEqual(1, result);
+        }
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Division(10, 3);
+            ClassicAssert.AreEqual(3, result);
+        }
+    }
+}
